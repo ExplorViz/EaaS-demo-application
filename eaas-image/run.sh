2f29e944f4f42280c3401d1495ff41fecef709de
Jan Erik Petersen <JanErikPetersen@web.de>
2020-03-12 06:09:57 +0100

    Reduce waiting times

--- eaas-image/run.sh
+++ eaas-image/run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -eu
 
-# Let analysis-service start
-sleep 30
+# Allow analysis-service some time to start
+sleep 10
 
 java-with-kieker -cp ".:BOOT-INF/classes/:BOOT-INF/lib/*" org.springframework.samples.petclinic.PetClinicApplication &
-
-# Allow server some time to start to avoid premature errors
-sleep 15
 
 # Now create some load on the server so we will have an interesting visualization
 ./create-load.sh
